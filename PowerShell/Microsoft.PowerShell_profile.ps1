d2c2efbf21d19da7215886749fd0e71b7f21df6b
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2023-08-01 03:23:48 -0500

    Added config for light colored theme

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,8 +9,6 @@ if ($IsWindows) {
     Set-Location -Path $env:SystemDrive\
 }
 
-Clear-Host
-
 $Error.Clear()
 
 $poshGitModule = Get-Module posh-git -ListAvailable
@@ -62,5 +60,48 @@ Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function ForwardWord
 Set-PSReadLineKeyHandler -Chord Ctrl+LeftArrow -Function BackwardWord
 Set-PSReadLineKeyHandler -Chord Ctrl+D2 -Function MenuComplete
 
+if ($host.ui.RawUI.WindowTitle -eq 'PowerShell ISE') {
+    $ISETheme = @{
+        Command                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+        Comment                  = $PSStyle.Foreground.FromRGB(0x006400)
+        ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF)
+        Default                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+        Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0)
+        Error                    = $PSStyle.Foreground.FromRGB(0xE50000)
+        InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1)
+        Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b)
+        ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00)
+        Member                   = $PSStyle.Foreground.FromRGB(0x000000)
+        Number                   = $PSStyle.Foreground.FromRGB(0x800080)
+        Operator                 = $PSStyle.Foreground.FromRGB(0x757575)
+        Parameter                = $PSStyle.Foreground.FromRGB(0x000080)
+        String                   = $PSStyle.Foreground.FromRGB(0x8b0000)
+        Type                     = $PSStyle.Foreground.FromRGB(0x008080)
+        Variable                 = $PSStyle.Foreground.FromRGB(0xff4500)
+        ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1)
+        Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
+    }
+    Set-PSReadLineOption -Colors $ISETheme
+
+    $PSStyle.Formatting.FormatAccent       = "`e[32m"
+    $PSStyle.Formatting.TableHeader        = "`e[32m"
+    $PSStyle.Formatting.ErrorAccent        = "`e[36m"
+    $PSStyle.Formatting.Error              = "`e[31m"
+    $PSStyle.Formatting.Warning            = "`e[33m"
+    $PSStyle.Formatting.Verbose            = "`e[33m"
+    $PSStyle.Formatting.Debug              = "`e[33m"
+    $PSStyle.Progress.Style                = "`e[33m"
+    $PSStyle.FileInfo.Directory            = $PSStyle.Background.FromRgb(0x2f6aff) +
+                                            $PSStyle.Foreground.BrightWhite
+    $PSStyle.FileInfo.SymbolicLink         = "`e[36m"
+    $PSStyle.FileInfo.Executable           = "`e[95m"
+    $PSStyle.FileInfo.Extension['.ps1']    = "`e[36m"
+    $PSStyle.FileInfo.Extension['.ps1xml'] = "`e[36m"
+    $PSStyle.FileInfo.Extension['.psd1']   = "`e[36m"
+    $PSStyle.FileInfo.Extension['.psm1']   = "`e[36m"
+}
+
 $StartupVars = @()
 $StartupVars = Get-Variable | Select-Object -ExpandProperty Name
+
+Clear-Host
