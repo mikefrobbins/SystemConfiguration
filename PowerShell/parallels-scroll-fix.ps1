e4301ec96e72a725f36df23556322f64a7b4b053
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-12-16 14:49:55 -0600

    Added parallels scroll fix

---
+++ PowerShell/parallels-scroll-fix.ps1
@@ -0,0 +1,4 @@
+﻿# Workaround for slow scrolling issue in Windows 11 on Parallels
+# For more info, see: https://mikefrobbins.com/2024/11/20/workaround-for-slow-scrolling-issue-in-windows-11-on-parallels/
+
+Get-PnpDevice -FriendlyName 'Parallels USB Mouse Synchronization Device' | Disable-PnpDevice -Confirm:$false -PassThru | Enable-PnpDevice -Confirm:$false
